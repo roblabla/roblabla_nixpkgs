afc22d80f06b885e9438224613c9b435483c60f3
roblabla <robinlambertz+dev@gmail.com>
2015-10-10 15:09:14 +0200

    Small improvements, using archlinux configure flags

--- rubinius/default.nix
+++ rubinius/default.nix
@@ -11,13 +11,23 @@ in
       sha256 = "16p5i7g02rjp5mkn544py1h9a485ajz5m12m51gqcdd059ri3d6n";
     };
     buildInputs = [ pkgs.ruby_2_2 pkgs.bundler pkgs.bison pkgs.zlib pkgs.openssl
-                    pkgs.llvm_35 pkgs.clang
+                    pkgs.llvm_35
                     # Used for tests
                     # pkgs.perl pkgs.procps pkgs.which
     ];
     patches = [ ./llvmconfig.patch ];
-    preConfigure = "bundle install --path vendor/bundle";
-    configureFlags = "--llvm-shared";
+    preConfigure = ''
+      bundle install --path vendor/bundle
+      configureFlags="$configureFlags --mandir $out/share/man
+                                      --mandir $out/share/man
+                                      --includedir $out/include/rubinius
+                                      --appdir $out/lib/rubinius
+                                      --gemsdir $out/lib/rubinius/gems
+                                      --llvm-shared
+                                      --preserve-prefix
+                                      --without-rpath
+                                      --bin-link bundle --bin-link bundler"
+    '';
     buildPhase = "rake build";
     checkPhase = "rake spec";
     installPhase = "rake install";
@@ -27,4 +37,13 @@ in
       "${pkgs.glibcLocales}/lib/locale/locale-archive";
     # Used for tests
     # TZDIR = "{pkgs.tzdata}/share/zoneinfo";
+    passthru = rec {
+      majorVersion = "2";
+      minorVersion = "2";
+      teenyVersion = "0";
+      patchLevel = "0";
+      rubyEngine = "rbx";
+      libPath = "lib/rubinius";
+      gemPath = "lib/rubinius/gems";
+    };
   }
