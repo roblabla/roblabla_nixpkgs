90da3124fd108834606459b3fff184b9ceedc217
roblabla <robinlambertz+dev@gmail.com>
2015-10-28 21:41:53 +0100

    Woops, wrong version in default.nix

--- norminette/default.nix
+++ norminette/default.nix
@@ -1,6 +1,6 @@
 { bundlerEnv }:
   bundlerEnv {
-    name = "norminette-1.0.0alpha";
+    name = "norminette-1.0.0.alpha1";
     gemset = ./gemset.nix;
     gemfile = ./Gemfile;
     lockfile = ./Gemfile.lock;
