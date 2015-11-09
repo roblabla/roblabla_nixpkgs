682f9f5d4a20a2ec6524287dd9d57ee24ff1a80a
roblabla <robinlambertz+dev@gmail.com>
2015-11-09 20:09:10 +0100

    Update version

--- norminette/default.nix
+++ norminette/default.nix
@@ -1,6 +1,6 @@
 { bundlerEnv }:
   bundlerEnv {
-    name = "norminette-1.0.0.alpha1";
+    name = "norminette-1.0.0.rc1.pre.0";
     gemset = ./gemset.nix;
     gemfile = ./Gemfile;
     lockfile = ./Gemfile.lock;
