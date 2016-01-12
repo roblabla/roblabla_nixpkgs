ba7803937ec0925da69b509dca03f9991167093e
roblabla <robinlambertz+dev@gmail.com>
2016-01-12 15:37:47 +0100

    Oops, typo in by-spec

--- vector/default.nix
+++ vector/default.nix
@@ -19,7 +19,7 @@ in
     deps = [ nodePackages.by-spec."classnames"."^2.1.2" nodePackages.by-spec."extract-text-webpack-plugin"."^0.9.1" nodePackages.by-spec."filesize"."^3.1.2" nodePackages.by-spec."flux"."~2.0.3" nodePackages.by-spec."gemini-scrollbar"."^1.3.0" nodePackages.by-spec."gfm.css"."^1.1.1" nodePackages.by-spec."highlight.js"."^9.0.0" nodePackages.by-spec."linkifyjs"."^2.0.0-beta.4" nodePackages.by-spec."matrix-js-sdk"."https://github.com/matrix-org/matrix-js-sdk.git#develop" nodePackages.by-spec."matrix-react-sdk"."https://github.com/matrix-org/matrix-react-sdk.git#develop" nodePackages.by-spec."modernizr"."^3.1.0" nodePackages.by-spec."q"."^1.4.1" nodePackages.by-spec."react"."^0.14.2" nodePackages.by-spec."react-dnd"."^2.0.2" nodePackages.by-spec."react-dnd-html5-backend"."^2.0.0" nodePackages.by-spec."react-dom"."^0.14.2" nodePackages.by-spec."react-gemini-scrollbar"."^2.0.1"
       nodePackages.by-spec."catw"."^1.0.1"
       nodePackages.by-spec."babel"."^5.8.23"
-      nodePackages.by-spec."webpack"."^.1.12.6"
+      nodePackages.by-spec."webpack"."^1.12.6"
     ];
     peerDependencies = [];
   }
